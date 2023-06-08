cdc647020a222a9e0fe6edb28dcbf4f829dccadc
ISHIKANT DUSOYE <110443129+Ishikant@users.noreply.github.com>
2023-06-08 11:07:11 -0400

    Update Reservation.js

--- front-end/src/reservations/Reservation.js
+++ front-end/src/reservations/Reservation.js
@@ -1,35 +1,4 @@
 import React, { useEffect, useState } from "react";
-
-function Reservation({ reservation, cancelHandler }) {
-
-  const [status, setStatus] = useState(reservation.status);
-  const [cancelled, setCancelled] = useState(false)
-
-  useEffect(() => {
-    setStatus(reservation.status)
-    if(reservation.status === "cancelled")
-      setCancelled(true);
-  }, [reservation.status])
-
-  function setToCanceled(){
-    
-    if(cancelHandler(reservation.reservation_id)){
-      setCancelled(!cancelled);
-      setStatus("cancelled");
-    }
-  }
-
-  return (
-
-    <tr>
-      <td>{reservation.reservation_id}</td>
-      <td>{reservation.reservation_date} {reservation.reservation_time}</td>
-      <td>{reservation.first_name}</td>
-      <td>{reservation.last_name}</td>
-      <td> {reservation.people}</td>
-      <td>{reservation.mobile_number}</td>
-      <td >{ status === 'cancelled' ? <p className="badge text-bg-danger">{status}</p>:<p className="badge text-bg-dark">{status}</p>}</td>
-      import React, { useEffect, useState } from "react";
 function Reservation({ reservation, cancelHandler }) {
   const [status, setStatus] = useState(reservation.status);
   const [cancelled, setCancelled] = useState(false)
@@ -95,12 +64,8 @@ function Reservation({ reservation, cancelHandler }) {
           </button>
         )}
       </td>
+      ;
     </tr>
   );
 }
-export default Reservation;
-    
-  );
-}
-
 export default Reservation;
